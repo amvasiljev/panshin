df5f8580746c1a6fc538516925d0157a7529054a
bikeol <a@mosam.ru>
2020-10-04 02:07:52 +0300

    src:

--- src/js/common.js
+++ src/js/common.js
@@ -435,3 +435,26 @@ function tabsChange(mobile) {
 
 
 }
+
+
+$('#feedback_file').on('change', function () {
+  var arrayFiles = this.files, // массив с выбранными файлами
+    formItem = this.parentNode, // родительский элемент, для того чтобы вставить список с файлами
+    listFiles = document.createElement('ul'), // список с файлами
+    li = ''; // файлы
+
+  // Если список с файлами уже вставлен в ДОМ, то удаляем его
+  if (formItem.querySelector('.feedback__filelist')) {
+    formItem.querySelector('.feedback__filelist').remove();
+  }
+
+  listFiles.className = 'feedback__filelist'; // 
+
+  for (var i = 0; i < arrayFiles.length; i++) {
+    li += '<li>' + arrayFiles[i].name + '</li>'; // <li>Имя файла</li>
+  }
+
+  listFiles.innerHTML = li;
+
+  formItem.appendChild(listFiles);
+});
